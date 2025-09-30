b0057f468c28c8d7fd6a407a48d4b8da54a7f62f
RadithyaNaufal <radithyanm@gmail.com>
2025-09-30 21:47:00 +0700

    Assignment 5 DONE

--- soccer_store/settings.py
+++ soccer_store/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "radithya-naufal-soccerstore.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://radithya-naufal-footballnews.pbp.cs.ui.ac.id"
+    "https://radithya-naufal-soccerstore.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
